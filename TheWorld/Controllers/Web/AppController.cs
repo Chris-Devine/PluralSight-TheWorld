ea53e4d52590a4385a5d3fceebbf5607d8ccd20b
Chris-Devine <chris.devine89@gmail.com>
2017-05-12 13:47:22 +0100

    MVC 6 part 2 - completed section now

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
 using TheWorld.ViemModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
         {
 
@@ -20,9 +31,23 @@ namespace TheWorld.Controllers.Web
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.AddModelError("Email", "We dont support AOL addresses");
+                ModelState.AddModelError("", "We dont support AOL addresses");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "the world", model.Message);
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
+
             return View();
         }
 
@@ -30,7 +55,6 @@ namespace TheWorld.Controllers.Web
 
         public IActionResult About()
         {
-
             return View();
         }
 
